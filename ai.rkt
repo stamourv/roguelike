5e46f5847d0f0c61b7773a983b472ca5a79f59a5
Vincent St-Amour <stamourv@racket-lang.org>
2011-03-06 20:08:42 -0500

    Move pathfinding to its own file.

--- ai.rkt
+++ ai.rkt
@@ -7,7 +7,8 @@
 (require "character.rkt"
          "monsters.rkt"
          "items.rkt"
-         "visibility.rkt")
+         "visibility.rkt"
+         "pathfinding.rkt")
 (provide (all-defined-out))
 
 (define (rush-behavior)
@@ -90,66 +91,3 @@
       ;; we did not move, increment idle counter
       (let ((b (monster-behavior monster)))
         (set-behavior-nb-turns-idle! b (+ (behavior-nb-turns-idle b) 1))))))
-
-;; simple pathfinding using A*
-(define (find-path g a b)
-  ;; grid of pairs (cost . previous)
-  (let* ((height  (grid-height g))
-	 (width   (grid-width g))
-	 (maximum (* width height)) ; arbitrarily high value
-	 (costs (empty-grid
-		 height width
-		 #:cell-fun (lambda (pos)
-                              (cons (if (walkable-cell? (grid-ref g pos))
-                                        maximum
-                                        #f) ; we can't even get there
-                                    #f)))))
-    (grid-set! costs a (cons 0 #f)) ; initialize
-    (let loop ((queue (list a))) ; list of positions
-      (if (null? queue)
-	  ;; we have found a path, we return its first step
-	  (let loop ((pos  b)
-		     (prev #f))
-	    (let ((parent (cdr (grid-ref costs pos))))
-	      (if parent
-		  (loop parent pos)
-		  prev)))
-	  (let* ((next (foldl (lambda (new best) ; least expensive neighbor
-			       (if (< (car (grid-ref costs new))
-				      (car (grid-ref costs best)))
-				   new
-				   best))
-			     (car queue)
-			     queue))
-		 (queue (remove next queue))
-		 (neighbors
-		  (filter
-		   (lambda (pos)
-		     (if (inside-grid? g pos)
-			 (cond
-			  ((car (grid-ref costs pos)) =>
-			   (lambda (cost)
-			     (let ((new-cost
-				    (+ (car (grid-ref costs next))
-				       ;; heuristic cost
-				       (distance pos b)
-				       ;; if we would pass through another
-				       ;; monster, take into account the number
-				       ;; of turns it has been stuck there, to
-				       ;; avoid congestion
-				       (let ((occ (cell-occupant
-						   (grid-ref g pos))))
-					 (if (and occ (monster? occ))
-					     (* (behavior-nb-turns-idle
-						 (monster-behavior occ))
-						5)
-					     0)))))
-			       (if (< new-cost cost)
-				   (begin
-				     (grid-set! costs pos (cons new-cost next))
-				     #t)
-				   #f))))
-			  (else #f))
-			 #f))
-		   (four-directions next))))
-	    (loop (append neighbors queue)))))))
---
+++ pathfinding.rkt
@@ -0,0 +1,69 @@
+#lang racket
+
+(require "utilities/grid.rkt"
+         "utilities/cell.rkt")
+(require "monsters.rkt")
+(provide find-path)
+
+;; simple pathfinding using A*
+(define (find-path g a b)
+  ;; grid of pairs (cost . previous)
+  (let* ((height  (grid-height g))
+	 (width   (grid-width g))
+	 (maximum (* width height)) ; arbitrarily high value
+	 (costs (empty-grid
+		 height width
+		 #:cell-fun (lambda (pos)
+                              (cons (if (walkable-cell? (grid-ref g pos))
+                                        maximum
+                                        #f) ; we can't even get there
+                                    #f)))))
+    (grid-set! costs a (cons 0 #f)) ; initialize
+    (let loop ((queue (list a))) ; list of positions
+      (if (null? queue)
+	  ;; we have found a path, we return its first step
+	  (let loop ((pos  b)
+		     (prev #f))
+	    (let ((parent (cdr (grid-ref costs pos))))
+	      (if parent
+		  (loop parent pos)
+		  prev)))
+	  (let* ((next (foldl (lambda (new best) ; least expensive neighbor
+			       (if (< (car (grid-ref costs new))
+				      (car (grid-ref costs best)))
+				   new
+				   best))
+			     (car queue)
+			     queue))
+		 (queue (remove next queue))
+		 (neighbors
+		  (filter
+		   (lambda (pos)
+		     (if (inside-grid? g pos)
+			 (cond
+			  ((car (grid-ref costs pos)) =>
+			   (lambda (cost)
+			     (let ((new-cost
+				    (+ (car (grid-ref costs next))
+				       ;; heuristic cost
+				       (distance pos b)
+				       ;; if we would pass through another
+				       ;; monster, take into account the number
+				       ;; of turns it has been stuck there, to
+				       ;; avoid congestion
+				       (let ((occ (cell-occupant
+						   (grid-ref g pos))))
+					 (if (and occ (monster? occ))
+					     (* (behavior-nb-turns-idle
+						 (monster-behavior occ))
+						5)
+					     0)))))
+			       (if (< new-cost cost)
+				   (begin
+				     (grid-set! costs pos (cons new-cost next))
+				     #t)
+				   #f))))
+			  (else #f))
+			 #f))
+		   (four-directions next))))
+	    (loop (append neighbors queue)))))))
