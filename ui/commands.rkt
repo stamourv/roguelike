2b024d2b4b89b4daff0b1e852b576eaaf1436177
Vincent St-Amour <stamourv@racket-lang.org>
2011-03-20 21:56:04 -0400

    Refactor input code.

---
+++ ui/commands.rkt
@@ -0,0 +1,141 @@
+#lang racket
+
+(require (only-in srfi/1 iota))
+(require "../utilities/terminal.rkt"
+         "../utilities/display.rkt"
+         "../utilities/grid.rkt")
+(require "../engine/cell.rkt"
+         "../engine/character.rkt"
+         "../engine/player.rkt"
+         "../engine/combat.rkt"
+         "../engine/items.rkt"
+         "../engine/common.rkt"
+         "../engine/visibility.rkt")
+(require "utilities.rkt")
+
+(provide (all-defined-out))
+
+(define (inventory)
+  (cursor-home)
+  (clear-to-bottom)
+  (display "Equipment:\n")
+  (for-each-equipped
+   (lambda (obj where)
+     (printf "~a~a\n" where (if obj (item-info obj) "")))
+   (character-equipment player))
+  (printf "\nAC: ~a\n" (get-armor-class player))
+  (display "\nInventory:\n")
+  (for-each (lambda (o) (printf "~a\n" (item-info o)))
+            (player-character-inventory player))
+  (let loop ((c #f))
+    (case c
+      ((#\e) (equip))
+      ((#\r) (take-off))
+      ((#\d) (cmd-drop))
+      ((#\q) #f)
+      (else (display "\ne: Equip\nr: Take off\nd: Drop\nq: Cancel\n")
+            (loop (read-char)))))
+  (clear-to-bottom))
+
+(define (pick-up)
+  (let* ((cell    (grid-ref (player-map player) (character-pos player)))
+         (items (cell-items cell)))
+    (choice items inventory-pick-up
+            "There is nothing to pick up." "Pick up what?" "Picked up ")))
+(define (cmd-drop)
+  (let ([items (player-character-inventory player)])
+    (choice items inventory-drop
+            "You have nothing to drop." "Drop what?" "Dropped ")))
+(define (equip)
+  (let ([items (filter (lambda (x) (equipable-item? x))
+                       (player-character-inventory player))])
+    (choice items inventory-equip
+            "You have nothing to equip." "Equip what?" "Equipped ")))
+(define (take-off)
+  (let* ([e     (character-equipment player)]
+         [items (filter (lambda (obj) (and obj (removable? obj)))
+                        (map car (equipment->list e)))])
+    (choice items inventory-take-off
+            "You have nothing to take off." "Take off what?" "Took off ")))
+(define (cmd-drink)
+  (let* ([e     (character-equipment player)]
+         [items (filter potion? ; currently, we can only drink potions
+                        (player-character-inventory player))]
+         [o     #f])
+    (choice items (lambda (item) (set! o item) (inventory-remove item))
+            "You have nothing to drink." "Drink what?" "Drank ")
+    ;; necessary to display the messages in the right order
+    (when o (drink-action o player))))
+
+
+(define (climb-stairs)
+  (let ((cell (grid-ref (player-map player) (character-pos player))))
+    (cond [(stairs-up? cell)   (go-up-stairs)]
+          [(stairs-down? cell) (go-down-stairs)]
+          [else (display "There are no stairs here.\n")])))
+
+(define (cmd-open)  (direction-command "Open"  open))
+(define (cmd-close) (direction-command "Close" close))
+
+
+(define (shoot)
+  (let* ((grid    (player-map player))
+         (weapon  (equipment-main-hand (character-equipment player)))
+         (targets (available-targets player))
+         (n       (length targets)))
+    (cond
+     ((not (ranged-weapon? weapon))
+      (display "I can't shoot with that.\n"))
+     ((null? targets)
+      (display "There is nothing to shoot.\n"))
+     (else
+      (let ((grid (let ((grid (grid-copy grid)))
+                    (for-each
+                     (lambda (m n) ;; TODO like choice, won't scale over 10
+                       (grid-set!
+                        grid
+                        (character-pos m)
+                        (new-display-cell
+                         (string-ref (number->string (+ n 1)) 0))))
+                     targets
+                     (iota n))
+                    grid)))
+        ;; show which monsters are which numbers
+        (cursor-home) ;; TODO taken from show-state
+        (clear-to-bottom)
+        (cursor-notification-head)
+        (for-each (lambda (m n)
+                    (printf-notification "~a: ~a\n"
+                                         (+ n 1) (character-name m)))
+                  targets
+                  (iota n))
+        (newline)
+        (printf-notification "q: Cancel\n")
+        ;; show the map with the target numbers
+        (cursor-home)
+        (printf "Floor ~a\n" (player-character-floor-no player))
+        (show-grid grid
+                   #:print-fun (visibility-show (player-view player)
+                                                (player-map  player)))
+        ;; choose a target
+        (let ((nb (read-number n)))
+          (when nb (ranged-attack player (list-ref targets nb)))))))))
+
+
+;; for debugging purposes
+(define (kill) ; insta-kill something
+  (direction-command "Kill"
+                     (lambda (grid cell player)
+                       (cond ((cell-occupant cell)
+                              => (lambda (occ)
+                                   (printf "Killed the ~a\n"
+                                           (character-name occ))
+                                   (remove-monster occ)))
+                             (else (display
+                                    "There is nothing to kill there.\n"))))))
+(define (reveal-map)
+  (let ((view (player-view player)))
+    (grid-for-each (lambda (p) (grid-set! view p 'visited)) view)))
+(define (god-mode)
+  (reveal-map)
+  (set-box! god-mode? #t))
---
+++ ui/help.rkt
@@ -0,0 +1,79 @@
+#lang racket
+
+(require "../utilities/terminal.rkt"
+         "../utilities/list.rkt"
+         "../utilities/grid.rkt"
+         "../utilities/display.rkt"
+         "../utilities/descriptions.rkt")
+(require "../engine/common.rkt"
+         "../engine/cell.rkt"
+         "../engine/player.rkt"
+         "../engine/character.rkt")
+
+(provide (all-defined-out))
+
+;; help features
+(define (describe-commands)
+  ;; TODO show all commands, and have a version that waits for one command
+  ;;  and says what it is
+  'TODO)
+
+(define (describe-one what)
+  (let ([type+desc (dict-ref descriptions-table what
+                             "I don't know what that is.")])
+    (if (pair? type+desc)
+        (format "~a" (cdr type+desc))
+        type+desc)))
+
+;; reads a character, and prints out the description
+(define (describe)
+  (displayln "Type the character you want me to describe.")
+  (echo-on)
+  (let ([what (read-char)])
+    (newline)
+    (displayln (describe-one what)))
+  (echo-off))
+
+(define (describe-all)
+  (let ([grouped (group-by-identical
+                  descriptions-table string<?
+                  ;; sort by category
+                  #:key (lambda (x) (symbol->string (cadr x))))])
+    (for ([g grouped])
+         ;; capitalize category
+         (let ([cat (symbol->string (cadar g))])
+           (printf "~a~as:\n"
+                   (char-upcase (string-ref cat 0))
+                   (substring cat 1)))
+         (for ([x g])
+              (print-sprite (car x))
+              (printf ": ~a\n"  (cddr x)))
+         (newline)))
+  (displayln "\nPress any key.")
+  (read-char)
+  (for ([i (in-range 30)]) (newline)))
+
+(define (info)
+  (let* ([grid (player-map player)]
+         [pos  (character-pos player)]
+         [cell (grid-ref grid pos)]
+         [occ  (cell-occupant cell)])
+    ;; temporarily remove occupant, so see beyond the player itself
+    (set-cell-occupant! cell #f)
+    (displayln (describe-one (show cell)))
+    (set-cell-occupant! cell occ)))
+
+(define (look)
+  ;; TODO have a moveable cursor, and when enter is pressed, display the info
+  ;;  of the location, pos is starting position of the cursor, if final cursor
+  ;;  position is outside visibility, say I can't see there
+  ;; TODO use the choose-direction command to control the cursor
+  (cursor-on)
+  ;; TODO maybe just be able to look at immediate squares, and just use
+  ;;  direction-command, but we might want to identify something before we
+  ;;  get closer (a monster, for example)
+  ;;   (set-cursor-on-grid grid pos)
+  (read-char)
+  ;; TODO implement the rest, and it seems that pressing l then an arrow
+  ;;  shows some weird text in the background about terminal options
+  (cursor-off))
